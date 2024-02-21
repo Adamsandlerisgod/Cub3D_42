b84871f3566b1be859d067666ff314454f3390ee
jusdal <justin.m.daly@gmail.com>
2024-02-21 20:06:01 +0700

    rename test map

--- src/main_justin.c
+++ src/main_justin.c
@@ -6,16 +6,16 @@
 /*   By: justindaly <justindaly@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 18:48:51 by jdaly             #+#    #+#             */
-/*   Updated: 2024/02/21 19:30:27 by justindaly       ###   ########.fr       */
+/*   Updated: 2024/02/21 20:05:10 by justindaly       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/raycast.h"
+// #include "../includes/raycast.h"
 
-int main(int argc, char *argv[])
-{
-	t_mapinfo	mapinfo;
+// int main(int argc, char *argv[])
+// {
+// 	t_mapinfo	mapinfo;
 
-	validate_cub(argc, argv, &mapinfo);
-	return (0);
-}
+// 	validate_cub(argc, argv, &mapinfo);
+// 	return (0);
+// }
