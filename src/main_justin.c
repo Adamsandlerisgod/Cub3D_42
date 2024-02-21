bcebf15a3001a7b5e907c4f8918c5d48e852fdd9
jusdal <justin.m.daly@gmail.com>
2024-02-21 20:05:27 +0700

    update justin_main validate_cub

--- src/main_justin.c
+++ src/main_justin.c
@@ -3,19 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   main_justin.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jdaly <jdaly@student.42.fr>                +#+  +:+       +#+        */
+/*   By: justindaly <justindaly@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 18:48:51 by jdaly             #+#    #+#             */
-/*   Updated: 2024/02/07 18:50:44 by jdaly            ###   ########.fr       */
+/*   Updated: 2024/02/21 19:30:27 by justindaly       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "../includes/raycast.h"
+#include "../includes/raycast.h"
 
-// int main(int argc, char *argv[])
-// {
-// 	t_mapinfo	mapinfo;
+int main(int argc, char *argv[])
+{
+	t_mapinfo	mapinfo;
 
-// 	validate_map(argc, argv, &mapinfo);
-// 	return (0);
-// }
+	validate_cub(argc, argv, &mapinfo);
+	return (0);
+}
