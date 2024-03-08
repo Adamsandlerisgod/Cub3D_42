01c8c7beed9c759d52dde617e000ca3d6e0eef2c
Wolf Hendrikse <whendrik@z3t5c1.42bangkok.com>
2024-03-08 19:52:33 +0700

    [NORM] Fix norm rc* and includes file

--- src/main.c
+++ src/main.c
@@ -6,27 +6,27 @@
 /*   By: whendrik <whendrik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 14:04:19 by whendrik          #+#    #+#             */
-/*   Updated: 2024/03/07 19:01:22 by whendrik         ###   ########.fr       */
+/*   Updated: 2024/03/08 19:49:27 by whendrik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/raycast.h"
 
-int main_loop(t_data *data)
+int	main_loop(t_data *data)
 {
-	move_avatar(data);	
+	move_avatar(data);
 	img_draw_background(data);
 	draw_ray(data);
 	draw_mini_map(data);
 	mlx_clear_window(data->mlx.mlx, data->mlx.mlx_win);
-	mlx_put_image_to_window(data->mlx.mlx, data->mlx.mlx_win,
-		 data->ray_to_draw.img, 0, 0);
+	mlx_put_image_to_window(data->mlx.mlx, data->mlx.mlx_win, \
+		data->ray_to_draw.img, 0, 0);
 	return (0);
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-	t_data	data;
+	t_data		data;
 	t_mapinfo	mapinfo;
 
 	ft_bzero(&data, sizeof(t_data));
@@ -34,11 +34,11 @@ int main(int ac, char **av)
 	if (validate_cub(ac, av, &mapinfo) != SUCCESS)
 		exit (1);
 	if (!(init_program(&data, &mapinfo)))
-		return(err_msg("" ,"Failed to initialize program", 0), 0);
+		return (err_msg("", "Failed to initialize program", 0), 0);
 	mlx_hook(data.mlx.mlx_win, 2, 1L << 0, key_press, &data);
 	mlx_hook(data.mlx.mlx_win, 3, 1L << 1, key_release, &data);
 	mlx_hook(data.mlx.mlx_win, 4, 1L << 2, mouse_press, &data);
-	mlx_hook(data.mlx.mlx_win, 5, 1L << 3, mouse_release, &data); 
+	mlx_hook(data.mlx.mlx_win, 5, 1L << 3, mouse_release, &data);
 	mlx_loop_hook(data.mlx.mlx, main_loop, &data);
 	mlx_loop(data.mlx.mlx);
 }
--- src/rc_assign_data.c
+++ src/rc_assign_data.c
@@ -6,7 +6,7 @@
 /*   By: whendrik <whendrik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 16:47:14 by whendrik          #+#    #+#             */
-/*   Updated: 2024/03/07 18:56:46 by whendrik         ###   ########.fr       */
+/*   Updated: 2024/03/08 19:47:46 by whendrik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,43 +24,40 @@ static void	assign_direction_hero(t_data *data, char dir)
 		data->facing_angle = 0;
 }
 
-
-
 static t_img	text_assign(t_data *data, char *path, int width, int height)
 {
 	t_img	texture;
 
 	texture.width = width;
 	texture.height = height;
-	texture.img = mlx_xpm_file_to_image(data->mlx.mlx, path,
+	texture.img = mlx_xpm_file_to_image(data->mlx.mlx, path, \
 		&texture.width, &texture.height);
 	if (texture.img == NULL)
 		return (texture);
-	texture.addr = mlx_get_data_addr(texture.img, &texture.bits_per_pixel,
+	texture.addr = mlx_get_data_addr(texture.img, &texture.bits_per_pixel, \
 		&texture.line_length, &texture.endian);
 	return (texture);
 }
 
-static bool assign_texture(t_data *data, t_mapinfo *map)
+static bool	assign_texture(t_data *data, t_mapinfo *map)
 {
-	data->textures.no = text_assign(data, map->no_path, 64, 64); 
+	data->textures.no = text_assign(data, map->no_path, 64, 64);
 	if (data->textures.no.img == NULL)
 		return (FALSE);
-	data->textures.so = text_assign(data, map->so_path, 64, 64); 
+	data->textures.so = text_assign(data, map->so_path, 64, 64);
 	if (data->textures.so.img == NULL)
 		return (FALSE);
-	data->textures.ea = text_assign(data, map->ea_path, 64, 64); 
+	data->textures.ea = text_assign(data, map->ea_path, 64, 64);
 	if (data->textures.so.img == NULL)
 		return (FALSE);
-	data->textures.we = text_assign(data, map->we_path, 64, 64); 
+	data->textures.we = text_assign(data, map->we_path, 64, 64);
 	if (data->textures.we.img == NULL)
 		return (FALSE);
 	return (TRUE);
-}	
+}
 
 bool	assign_data(t_data *data, t_mapinfo *map)
 {
-
 	if (assign_texture(data, map) == FALSE)
 		return (FALSE);
 	data->mlx.win_width = WIDTH;
